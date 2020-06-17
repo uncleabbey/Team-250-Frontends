807a5152fba78efa036288192f0c3e1cc1bf9000
Pauliski <48366206+Pauliski@users.noreply.github.com>
2020-06-17 18:41:52 +0100

    Revert "Revert "Revert "To merge develop branch to master branch"""

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// eslint-disable-next-line sort-imports
-import App from './app';
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
